1c2dcd93718cd15e41efea4f8b2ce49b23dfc6b1
davidGrine <grindavid8@gmail.com>
2026-09-17 14:07:20 +0300

    DevHUB

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,14 +1,9 @@
 import { NestFactory } from '@nestjs/core'
-
 import { AppModule } from './app.module'
-
 import * as path from 'path'
-
 import * as fs from 'fs'
 
-
 const start = async () => {
-
   const app = await NestFactory.create(AppModule)
 
   const staticPath = path.resolve(__dirname, 'static')
@@ -21,18 +16,19 @@ const start = async () => {
 
   const PORT = process.env.PORT || 5000
 
-  const FRONTEND_URL =
-    process.env.FRONTEND_URL 
-
   app.enableCors({
-    origin: FRONTEND_URL,
-    credentials: true
+    origin: [
+      'http://localhost:3000',
+      'https://dev-hub-gules.vercel.app'
+    ],
+    credentials: true,
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
   })
 
   await app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server has been started on port ${PORT}`)
   })
-
 }
 
 start()
