cec6f37e6030459e64ada84c4031e69dcd4a4ebf
davidGrine <grindavid8@gmail.com>
2026-09-17 13:13:00 +0300

    DevHUB

--- server/config/config.js
+++ server/config/config.js
@@ -1,39 +1,34 @@
 require('dotenv').config()
 
 module.exports = {
-	development: {
-		use_env_variable: 'DATABASE_URL',
-		dialect: 'postgres',
-		dialectOptions: {
-			ssl: {
-				require: true,
-				rejectUnauthorized: false
-			}
-		}
-	},
+  development: {
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  },
 
-	test: {
-		username: process.env.DB_USERNAME,
-		password: process.env.DB_PASSWORD,
-		database: process.env.DB_NAME,
-		host: process.env.DB_HOST,
-		port: Number(process.env.DB_PORT),
-		dialect: 'postgres'
-	},
+  test: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    dialect: 'postgres',
+  },
 
-	production: {
-		username: process.env.POSTGRES_USER,
-		password: process.env.POSTGRES_PASSWORD,
-		database: process.env.POSTGRES_DB,
-		host: process.env.POSTGRES_HOST,
-		port: process.env.POSTGRES_PORT,
-		dialect: 'postgres',
-
-		dialectOptions: {
-			ssl: {
-				require: true,
-				rejectUnauthorized: false
-			}
-		}
-	}
+  production: {
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  },
 }
